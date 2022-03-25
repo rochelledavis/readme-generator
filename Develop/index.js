5f4f160a7a5ef1ca0cbf89442df512e92f718784
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-03-24 22:53:42 -0600

    initial setup

---
+++ Develop/index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+
+    // const inquirer = require('inquirer');
+    // const generatePage = require('./src/readme-template');
+    // const { writeFile, copyFile } = require('./utils/generateMarkdown');
+
+
+
+// TODO: Create an array of questions for user input
+    //Description, Table of Contents, Installation, Usage, License(badge), Contributing, Tests, and 
+    //Questions - Links Github account
+        //email address
+    //Table of Contents links to appropriate sections
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
