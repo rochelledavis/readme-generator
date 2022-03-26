8b7dcb21ad155d5781c23b0690e0429fdc771fc5
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-03-25 18:16:29 -0600

    questions created

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 
-    // const inquirer = require('inquirer');
-    // const generatePage = require('./src/readme-template');
-    // const { writeFile, copyFile } = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+// const generateMarkdown = require('./src/readme-template');
+// const { writeFile, copyFile } = require('./utils/generateMarkdown');
 
 
 
@@ -11,13 +11,121 @@
     //Questions - Links Github account
         //email address
     //Table of Contents links to appropriate sections
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions for your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your installation instructions!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide a usage instructions for your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your usage instructions!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributers',
+            message: 'Please provide the names of those who contributed to your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter all contributors!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please provide test instructions for your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter test instructions!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please provide your GitHub username.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please provide your email address.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your email!');
+                    return false;
+                }
+            }
+        },
+    ]);
+};
 
+questions();
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
